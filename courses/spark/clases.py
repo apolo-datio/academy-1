fd36d2dffd63429a3bf727bad8465d0508120b33
Ricardo Bravo <rbravo@datiobd.com>
2017-01-26 23:11:45 +0100

    Add files via upload

---
+++ courses/spark/clases.py
@@ -0,0 +1,7 @@
+class Cliente(object):
+    def __init__(self, linea):
+        self.dni, self.nombre, self.direccion = linea.split(',')
+
+class Tarjeta(object):
+    def __init__(self, linea):
+        self.dni, self.nombre, self.tipo_tarjeta, self.num_tarjeta = linea.split(',')
