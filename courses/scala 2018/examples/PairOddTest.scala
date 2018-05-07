0627d01287f1e2c2fa3de0831491d7a512e86b09
nataliecampo <ncampo@datiobd.com>
2018-05-07 11:22:07 +0200

    Add files via upload

---
+++ courses/scala 2018/examples/PairOddTest.scala
@@ -0,0 +1,42 @@
+package com.datio.examples
+
+import com.datio.exercises.PairOdd
+import org.scalatest.{Matchers, WordSpec}
+
+class PairOddTest extends WordSpec with Matchers{
+
+  val instance: PairOdd = new PairOdd{}
+
+  "Check pair cases" should{
+    "with positive pair number we should get true" in{
+      instance.isPair(44) shouldBe true
+    }
+    "with negative pair number we should get true" in{
+      instance.isPair(-4) shouldBe true
+    }
+    "with positive odd number we should get false" in{
+      instance.isPair(11) shouldBe false
+    }
+    "with postive odd number we should get false" in{
+      instance.isPair(27) shouldBe false
+    }
+    "with negative odd number we should get false" in{
+      instance.isPair(-1) shouldBe false
+    }
+  }
+  "Check odd cases" should{
+    "with positive odd number we should get true" in{
+      instance.isOdd(1) shouldBe true
+    }
+    "with negative odd number we should get true" in{
+      instance.isOdd(-3) shouldBe true
+    }
+    "with postive pair number we should get false" in{
+      instance.isOdd(22) shouldBe false
+    }
+    "with negative pair number we should get false" in{
+      instance.isOdd(-46) shouldBe false
+    }
+  }
+
+}
